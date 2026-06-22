60a6322982a9715154cb096376167fc6642327b3
extrblock69 <extrfortest4@hotmail.com>
2026-06-22 07:32:20 +0530

    refactor(server): remove unused ESM path utilities

--- server.ts
+++ server.ts
@@ -2,13 +2,9 @@ import express from "express";
 import path from "path";
 import fs from "fs";
 import * as dotenv from "dotenv";
-import { fileURLToPath } from "url";
 import cors from "cors";
 
 dotenv.config();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
